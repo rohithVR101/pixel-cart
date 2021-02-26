bd36f10464540e7ead52052ad82926ef850f3864
Sreelekha Raman <sreelekha2031999@gmail.com>
2021-02-26 16:39:04 +0530

    login copied

--- src/app/components/authentication/login/login.component.ts
+++ src/app/components/authentication/login/login.component.ts
@@ -1,18 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+// import { Component, OnInit } from '@angular/core';
+// import { FormControl, Validators } from '@angular/forms';
+
+// @Component({
+//   selector: 'app-login',
+//   templateUrl: './login.component.html',
+//   styleUrls: ['./login.component.css'],
+// })
+// export class LoginComponent implements OnInit {
+//   email = new FormControl('', [Validators.required, Validators.email]);
+
+//   constructor() {}
+
+//   ngOnInit(): void {}
+
+//   getErrorMessage() {
+//     if (this.email.hasError('required')) {
+//       return 'You must enter a value';
+//     }
+
+//     return this.email.hasError('email') ? 'Not a valid email' : '';
+//   }
+// }
+import { Component } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-root',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+// <<<<<<< HEAD
+  title = 'pixel-cart';
+  
   email = new FormControl('', [Validators.required, Validators.email]);
-
-  constructor() {}
-
-  ngOnInit(): void {}
-
   getErrorMessage() {
     if (this.email.hasError('required')) {
       return 'You must enter a value';
@@ -20,4 +41,7 @@ export class LoginComponent implements OnInit {
 
     return this.email.hasError('email') ? 'Not a valid email' : '';
   }
+  hide = true;
+  title = 'PIXEL CART';
+// >>>>>>> a6c90d13f59e884dc44c79f6a2156ce5edc8d851
 }
